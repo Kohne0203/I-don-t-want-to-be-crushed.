63d8a7c127002c98f9ce6f6900ee2c80ad1fe54f
Ko W <bohneundbean@gmail.com>
2020-04-19 14:41:29 +0900

    Set WaitTime for FallingObject
    落下オブジェクトに予兆動作を追加

--- a day before deadline/Assets/Scripts/FallingObjectController.cs
+++ a day before deadline/Assets/Scripts/FallingObjectController.cs
@@ -5,16 +5,26 @@ using UnityEngine;
 public class FallingObjectController : MonoBehaviour
 {
     public float fallSpeed = 5.0f;
+    public float waitTime = 3;
+    public Rigidbody rb;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb.GetComponent<Rigidbody>();
+        rb.isKinematic = true;
+        Invoke("Fall", waitTime);
     }
 
     // Update is called once per frame
     void Update()
     {
+       
+    }
+
+    private void Fall()
+    {
+        rb.isKinematic = false;
         transform.Translate(Vector3.down * Time.deltaTime * fallSpeed);
     }
 
